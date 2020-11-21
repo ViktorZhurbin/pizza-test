b60a4ead42d3cc545477f35e0488fe6d939a4eda
Viktor Zhurbin <vzhurbin@gmail.com>
2020-11-21 21:55:17 +0300

    ✨ add user context

---
+++ src/contexts/User.tsx
@@ -0,0 +1,28 @@
+import { createContext, useEffect, useState } from 'react';
+import { useSession } from 'next-auth/client';
+import { fetchUser } from '@/modules/user/services';
+import { UserType } from '@/modules/user/typings';
+
+export const UserContext = createContext<UserType | undefined>(undefined);
+
+export const UserProvider: React.FC = ({ children }) => {
+    const [session] = useSession();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        if (session) {
+            fetchUser().then(({ data }) => {
+                // localStorage.removeItem(CART_KEY);
+                setUser(data);
+            });
+        }
+    }, [session]);
+
+    console.log('user', user);
+
+    return (
+        <UserContext.Provider value={{ ...user }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
 import { CurrencyProvider } from '@/contexts/Currency';
+import { UserProvider } from '@/contexts/User';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
         <Provider session={pageProps.session}>
-            <CurrencyProvider>
-                <Component {...pageProps} />
-            </CurrencyProvider>
+            <UserProvider>
+                <CurrencyProvider>
+                    <Component {...pageProps} />
+                </CurrencyProvider>
+            </UserProvider>
         </Provider>
     );
 };
