0b1f088f6411b5c320324130e239e870e7923540
Viktor Zhurbin <vzhurbin@gmail.com>
2020-11-22 22:06:50 +0300

    🐛 hide checkout button with empty cart

--- src/modules/user/pages/Cart/Cart.tsx
+++ src/modules/user/pages/Cart/Cart.tsx
@@ -1,15 +1,20 @@
 import { Button } from '@/components/Button';
 import { Layout } from '@/components/Layout';
+import { useUser } from '@/hooks/useUser';
 
 import { Cart } from '../../components/Cart';
 
 export const CartPage: React.FC = () => {
+    const { cart } = useUser();
+
     return (
         <Layout
             stickyItem={
-                <Button color="green" route="/checkout">
-                    Checkout
-                </Button>
+                cart.quantity ? (
+                    <Button color="green" route="/checkout">
+                        Checkout
+                    </Button>
+                ) : null
             }
         >
             <Cart />
