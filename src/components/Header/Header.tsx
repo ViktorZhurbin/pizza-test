e2fe3dcb58d9d07c013010759c64a542fea4d494
Viktor Zhurbin <vzhurbin@gmail.com>
2020-11-22 22:47:25 +0300

    🐛 fix header content shift

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,18 +7,15 @@ import { LOCALE } from '@/constants';
 export const Header: React.FC = () => {
     const { locale, setLocale } = useContext(LocaleContext);
     const [session, loading] = useSession();
+    const AuthButton = !session ? (
+        <button onClick={() => signIn()}>Sign in</button>
+    ) : (
+        <button onClick={() => signOut()}>Sign out</button>
+    );
 
     return (
         <header className={styles.header}>
-            {!loading && (
-                <div className={styles.auth}>
-                    {!session ? (
-                        <button onClick={() => signIn()}>Sign in</button>
-                    ) : (
-                        <button onClick={() => signOut()}>Sign out</button>
-                    )}
-                </div>
-            )}
+            <div className={styles.auth}>{!loading && AuthButton}</div>
             <select value={locale} onChange={(e) => setLocale(e.target.value)}>
                 <option value={LOCALE.de_DE}>EUR</option>
                 <option value={LOCALE.en_US}>USD</option>
